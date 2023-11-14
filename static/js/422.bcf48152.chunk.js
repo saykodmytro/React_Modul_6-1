4df3083b9e0364506f927b86b518e739aaa0f59b
saykodmytro <64913341+saykodmytro@users.noreply.github.com>
2023-11-14 20:57:52 +0000

    Deploying to gh-pages from @ saykodmytro/React_Modul_6-1@cb192b5098d0ddd9b42dcf8651b729f42277a19c 🚀

---
+++ static/js/422.bcf48152.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkReact_Modul_6_1=self.webpackChunkReact_Modul_6_1||[]).push([[422],{8422:function(e,s,t){t.r(s);t(2791);var u=t(7689),n=t(184);s.default=function(){var e=(0,u.UO)().postId;return(0,n.jsxs)("div",{children:["PostsComments: ",e]})}}}]);
+//# sourceMappingURL=422.bcf48152.chunk.js.map
