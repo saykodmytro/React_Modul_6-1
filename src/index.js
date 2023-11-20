9b3b72bb12a601eb8802609d99c05ae46e855691
Dmytro Sayko <saykodmytro@icloud.com>
2023-11-20 17:46:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Provider } from 'react-redux';
 import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ModalContextProvider>
+  <ModalContextProvider basename="/React_Modul_6-1">
     <BrowserRouter>
       {/* підключаємо наш додаток до сховища */}
       <Provider store={store}>
